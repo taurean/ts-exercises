cbe289271775706528ae9af945e27b4538b1875b
Taurean Bryant <later@taurean.work>
2025-05-24 16:09:02 -0700

    finishing challenge 2

--- src/intro/challenge2.ts
+++ src/intro/challenge2.ts
@@ -10,5 +10,6 @@
  */
 
 export const buildSentence = (word1: string, word2: string, word3: string) => {
-    return `${word1} ${word2} ${word3}.`;
+    const transformedWord1 = word1.charAt(0).toUpperCase() + word1.slice(1);
+    return `${transformedWord1} ${word2} ${word3}.`;
 };
